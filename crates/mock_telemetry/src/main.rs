2d4467455e7f632bed827a2469188d54e0337a91
Robin Schroer <git@sulami.xyz>
2023-05-02 17:18:07 +0900

    Update the mock telemetry generator with the latest lib changes

--- crates/mock_telemetry/src/main.rs
+++ crates/mock_telemetry/src/main.rs
@@ -3,13 +3,14 @@ use std::path::PathBuf;
 use std::thread;
 use std::time::{Duration, Instant};
 
-use embedded_imu::{telemetry::TelemetryReporter, transport::encode};
+use anyhow::Result;
+use embedded_imu::{log::Log, telemetry::TelemetryReporter, transport::encode};
 use random::Source;
 use serial_core::BaudRate::*;
 use serial_core::SerialPort;
 use serial_unix::TTYPort;
 
-fn main() {
+fn main() -> Result<()> {
     let path = PathBuf::from("/dev/ttys004");
     let mut rand = random::default(42);
     let mut reporter = TelemetryReporter::<10>::new();
@@ -20,20 +21,19 @@ fn main() {
     let mut counter = 0;
 
     loop {
-        assert!(reporter.record("ctr", counter as f32));
-        assert!(reporter.record("volt", 3.3 + rand.read::<f32>()));
-        assert!(reporter.record("gx", 0.5 - rand.read::<f32>()));
-        assert!(reporter.record("gy", 0.5 - rand.read::<f32>()));
-        assert!(reporter.record("gz", 9.3 + rand.read::<f32>()));
-        assert!(reporter.record("vot", (Instant::now() - start).as_secs_f32()));
-        assert!(reporter.record("sin", (Instant::now() - start).as_secs_f32().sin()));
-        assert!(reporter.record("cos", (Instant::now() - start).as_secs_f32().cos()));
-        assert!(reporter.record("tan", (Instant::now() - start).as_secs_f32().tan()));
+        reporter.record("ctr", counter as f32)?;
+        reporter.record("volt", 3.3 + rand.read::<f32>())?;
+        reporter.record("gx", 0.5 - rand.read::<f32>())?;
+        reporter.record("gy", 0.5 - rand.read::<f32>())?;
+        reporter.record("gz", 9.3 + rand.read::<f32>())?;
+        reporter.record("vot", (Instant::now() - start).as_secs_f32())?;
+        reporter.record("sin", (Instant::now() - start).as_secs_f32().sin())?;
+        reporter.record("cos", (Instant::now() - start).as_secs_f32().cos())?;
+        reporter.record("tan", (Instant::now() - start).as_secs_f32().tan())?;
         let report = reporter.report();
-        let mut buf = [0u8; 1024];
-        encode(&report, &mut buf);
-        let _ = tty.write(&buf).expect("failed to write telemetry");
-        let _ = tty.flush();
+        tty.write_all(encode(&report, &mut [0u8; 1024])?)?;
+        tty.write_all(encode(&Log::info("sent report"), &mut [0u8; 128])?)?;
+        tty.flush()?;
         counter += 1;
         thread::sleep(Duration::from_millis(50));
     }
